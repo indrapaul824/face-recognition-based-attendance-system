b2b9520cb00d4162688205892af530b065bdf955
IndraP24 <indrashis985@gmail.com>
2023-05-21 21:29:45 +0530

    Update addresses based on linux file system

---
+++ Attendance-System-Project/Deployment/Home_Page.py
@@ -0,0 +1,8 @@
+import streamlit as st
+from datetime import datetime
+import pandas as pd
+
+st.set_page_config(
+    page_title="Attendance System", page_icon="📊", layout="wide"
+)
+st.image("logo.png")
