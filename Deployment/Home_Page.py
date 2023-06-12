e0f9986817fe57510bc138439b3c3d909e160a4f
IndraP24 <indrashis985@gmail.com>
2023-06-12 23:58:17 +0530

    Update tab orientation

--- Deployment/Home_Page.py
+++ Deployment/Home_Page.py
@@ -13,23 +13,24 @@ st.title("🔎 View Attendance")
 st.write("---")
 path = r"./Attendance/"
 # Select options for manual and camera attendance
-tab1, tab2 = st.tabs(["Manual Attendance", "Camera Attendance"])
+tab1, tab2 = st.tabs(["Camera Attendance", "Manual Attendance"])
 with tab1:
-    d_l = glob.glob(path + "Manual/*.csv")
-    for i in range(len(d_l)):
-        d_l[i] = d_l[i].split("/")[-1].split(".")[0]
-    sheet = st.selectbox("Select Date", d_l)
-    df = pd.read_csv(path+"Manual/"+str(sheet)+".csv")
-    st.table(df)
-    st.write("---")
-    st.write("###")
-
-with tab2:
     d_l = glob.glob(path + "With_Camera/*.csv")
     for i in range(len(d_l)):
         d_l[i] = d_l[i].split("/")[-1].split(".")[0]
     sheet = st.selectbox("Select Date", d_l)
     df = pd.read_csv(path+"With_Camera/"+str(sheet)+".csv")
+    st.table(df)
+    st.write("---")
+    st.write("###")
+    
+
+with tab2:
+    d_l = glob.glob(path + "Manual/*.csv")
+    for i in range(len(d_l)):
+        d_l[i] = d_l[i].split("/")[-1].split(".")[0]
+    sheet = st.selectbox("Select Date", d_l)
+    df = pd.read_csv(path+"Manual/"+str(sheet)+".csv")
     st.table(df)
     st.write("---")
     st.write("###")
