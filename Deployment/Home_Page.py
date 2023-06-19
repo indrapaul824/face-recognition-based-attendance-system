319da33d8bbafb3487c4b06f0789cd9c2ce83493
IndraP24 <indrashis985@gmail.com>
2023-06-19 22:39:19 +0530

    Update home page

--- Deployment/Home_Page.py
+++ Deployment/Home_Page.py
@@ -6,7 +6,23 @@ import glob
 st.set_page_config(
     page_title="Attendance System", page_icon="📊", layout="wide"
 )
-# st.image("logo.png")
+# Details of the project
+st.title("📊 Attendance System")
+# Project Details
+st.markdown("Welcome to the Attendance System application. This system allows you to efficiently manage and track attendance for various events, classes, or meetings.")
+st.markdown("With its intuitive interface and powerful features, you can easily capture attendance using camera or manual input, view attendance records, and generate insightful visualizations.")
+
+# Additional project details
+st.markdown("Project Features:")
+st.markdown("- View Attendance: Select and view attendance data for different dates and attendance modes.")
+st.markdown("- Camera Attendance: Capture attendance using facial recognition from live camera feed.")
+st.markdown("- Manual Attendance: Record attendance manually for situations where camera attendance is not available.")
+st.markdown("- Attendance Visualization: Generate visualizations and insights from attendance data.")
+
+# Add any other relevant information about the project, such as its purpose, benefits, or key functionality.
+
+# End the project details section with a separator
+st.write("---")
 
 # functionalities for selecting the available sheets and viewing the attendance data as well as providing visualizations
 st.title("🔎 View Attendance")
